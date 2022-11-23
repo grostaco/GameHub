3b8c7c4e69266d0ecf439c99f31a60e3a4ffd8a9
grostaco <grostaco@gmail.com>
2022-11-24 02:18:32 +0700

    feat: add build scripts

---
+++ microservices/src/main.rs
@@ -0,0 +1,32 @@
+use std::{env, path::Path, process::Command};
+
+// For building and deploying to AWS
+// Requires a [lab] credentials in aws config
+fn main() {
+    dotenv::dotenv().unwrap();
+
+    let manifest = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+
+    let manifest_dir = Path::new(&manifest);
+
+    let mut args = env::args().skip(1);
+    let iam_role = args.next();
+
+    Command::new("cargo")
+        .args(&["lambda", "build", "--workspace"])
+        .current_dir(manifest_dir)
+        .status()
+        .unwrap();
+
+    let mut deploy = Command::new("cargo");
+    deploy
+        .args(&["lambda", "deploy", "gamehub-auth"])
+        .args(["--profile", "lab"]);
+
+    if let Some(iam_role) = iam_role {
+        println!("Deploying with IAM role: {iam_role}");
+        deploy.args(["--iam-role", &iam_role]);
+    }
+
+    deploy.current_dir(manifest_dir).status().unwrap();
+}
