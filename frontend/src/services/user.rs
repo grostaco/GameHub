bcfef65b41c1b74e69b2ab8fa0a42ac231c82570
grostaco <grostaco@gmail.com>
2022-12-13 10:17:58 +0700

    feat: patch request

--- frontend/src/services/user.rs
+++ frontend/src/services/user.rs
@@ -13,8 +13,22 @@ pub struct UserInfoResponse {
     pub games_played: Vec<String>,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct UserPatchRequest {
+    pub username: Option<String>,
+    pub bio: Option<String>,
+    pub avatar: Option<String>,
+}
+
 pub async fn get_user_info(jwt: &str) -> Result<UserInfoResponse> {
     let mut header_map = HeaderMap::new();
     header_map.insert("Authorization", HeaderValue::from_str(jwt).unwrap());
     request!(get -> "/users/@me" => header_map).await
+}
+
+pub async fn patch_user_info(jwt: &str, patch_request: &UserPatchRequest) -> Result<()> {
+    let mut header_map = HeaderMap::new();
+    header_map.insert("Authorization", HeaderValue::from_str(jwt).unwrap());
+    request!(patch -> "/users/@me" ; patch_request => header_map).await?;
+    Ok(())
 }
