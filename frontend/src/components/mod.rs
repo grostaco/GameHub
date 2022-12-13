d2cdd20656e906ce41975c425d3ef594f3948c8c
grostaco <grostaco@gmail.com>
2022-12-13 10:19:13 +0700

    feat: add textarea

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,9 +1,11 @@
 mod games;
 mod loading;
 mod nav;
+mod text_area;
 mod text_input;
 
 pub use games::Games;
 pub use loading::Loading;
 pub use nav::Nav;
+pub use text_area::TextArea;
 pub use text_input::TextInput;
